efb3cd8c1602279f82c04752ee4715efe0f2eb22
samharshe <samharshe29@gmail.com>
2023-08-29 22:57:31 -0400

    finished coding along with the video. i feel like i am learning a language and i just watched an hourlong debate in it. i am more familiar but still very far from mastery

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"))
+app.use(express.urlencoded( extended: true))
+
+app.set('view engine', 'ejs')
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter)
+
+app.listen(3000)
